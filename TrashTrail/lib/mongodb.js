5cdf10bc3ab9fb62e57b61d5861668ea1e010290
kpaswan9999-cp <kpaswan9999@gmail.com>
2026-04-04 17:48:09 +0530

    deploy: add environment variables and fix build crash

--- TrashTrail/lib/mongodb.js
+++ TrashTrail/lib/mongodb.js
@@ -2,10 +2,6 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
 /**
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections from growing exponentially
@@ -20,6 +16,10 @@ if (!cached) {
 async function connectDB() {
   if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
   }
 
   if (!cached.promise) {
